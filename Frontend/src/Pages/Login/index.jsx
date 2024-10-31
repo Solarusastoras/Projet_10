f669e43520364ac9af770453e34dbcca34a7919b
Solarusastoras <micformweb@gmail.com>
2024-10-31 19:51:48 +0100

    login

--- Frontend/src/Pages/Login/index.jsx
+++ Frontend/src/Pages/Login/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./_login.scss";
 import Form from "../../Components/Form/index.jsx";
 import Header from "../../Components/Header";
-import Userlogin from "../../img/user.webp"
+import Userlogin from "../../img/user.webp";
 
 const Login = () => {
   return (
@@ -11,15 +11,16 @@ const Login = () => {
       <main className="main bg-dark">
         <section className="sign-in-content">
           <div className="circle_user_login_bk">
-        <img
-    className="user_icon_login"
-    src={Userlogin}
-    alt="User Logo"
-    style={{
-        filter: 'brightness(0) saturate(100%) invert(100%) sepia(0%) saturate(7500%) hue-rotate(188deg) brightness(119%) contrast(119%)'
-    }}
-/>
-</div>  
+            <img
+              className="user_icon_login"
+              src={Userlogin}
+              alt="User Logo"
+              style={{
+                filter:
+                  "brightness(0) saturate(100%) invert(100%) sepia(0%) saturate(7500%) hue-rotate(188deg) brightness(119%) contrast(119%)",
+              }}
+            />
+          </div>
           <h1>Sign In</h1>
           <Form />
         </section>
